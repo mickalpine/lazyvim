bf44f067d3940918ed14ba0820e1a424fc629d88
Jordan McAlpine <jordan@mcalpine.dev>
2023-03-26 21:55:09 -0400

    added: keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Comment
+map("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", { desc = "Comment Line" })
+map(
+  "x",
+  "<leader>/",
+  "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+  { desc = "Comment Block" }
+)
+-- Press jk fast to enter
+map("i", "jk", "<ESC>", { desc = "Quick escape." })
+
+-- ToggleTerm
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+local opts = { noremap = true }
+map("n", "<leader>tf", "<cmd>ToggleTerm direction=float size=40<CR>", opts)
+map("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical size=40<CR>", opts)
+map("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal size=10<CR>", opts)
